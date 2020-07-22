ad3544a5a7c0f1b5a653bb583aa6c490acf8feaa
L200150050 <bintangindra.lol@gmail,com>
2020-07-21 21:40:22 -0700

    update database connection

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +14,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
-        factory(App\produk::class, 30)->create();
+        DB::table('users')->insert([
+            'nama_u' => 'susanto',
+            'alamat_u' => 'solo baru',
+            'email' => 'susanto@tanto.com',
+            'password' => Hash::make('keanureeve'),
+            'no_hp' => '082233123212',
+            'role' => 'admin',
+            'aktif' => 1,
+        ]);
     }
 }
